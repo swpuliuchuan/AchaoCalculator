bcb7913abced6b300c50ea10c906ce9058fa2131
swpuliuchuan <2117979238@qq.com>
2019-09-18 20:32:40 +0800

    第一次提交

---
+++ SoftwareE/Tostring.Tests/RanTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp9;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9.Tests
+{
+    [TestClass()]
+    public class RanTests
+    {
+        [TestMethod()]
+        public void getOperatorTest()
+        {
+            Ran rt1 = new Ran();
+            Random rand = new Random();
+            int d = rand.Next(4);
+            char c = Ran.getOperator();
+            char f;
+            switch (d)
+            {
+                case 0:
+                    d = '+';
+                    break;
+                case 1:
+                    d = '-';
+                    break;
+                case 2:
+                    d = '*';
+                    break;
+                case 3:
+                    d = '/';
+                    break;
+            }
+            Assert.AreEqual(c, d);
+        }
+    }
+}
